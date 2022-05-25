9781b5f1ea52aecb4ce64ccd1ee96d79869ac8d7
Marcin Gębala <maarcin.gebala@gmail.com>
2022-05-25 17:05:48 +0200

    Add tax configuration queries

---
+++ saleor/graphql/tax/types.py
@@ -0,0 +1,33 @@
+import graphene
+
+from ...tax import models
+from ..channel.types import Channel
+from ..core.connection import CountableConnection
+from ..core.descriptions import ADDED_IN_35, PREVIEW_FEATURE
+from ..core.types import ModelObjectType
+
+
+class TaxConfiguration(ModelObjectType):
+    channel = graphene.Field(
+        Channel,
+        description="A channel to which the tax configuration applies to.",
+        required=True,
+    )
+    charge_taxes = graphene.Boolean(
+        description="Determines whether taxes are charged in the given channel.",
+        required=True,
+    )
+    display_gross_prices = graphene.Boolean(description="", required=True)
+    prices_entered_with_tax = graphene.Boolean(description="", required=True)
+
+    class Meta:
+        description = (
+            "Channel-specific tax configuration." + ADDED_IN_35 + PREVIEW_FEATURE
+        )
+        interfaces = [graphene.relay.Node]
+        model = models.TaxConfiguration
+
+
+class TaxConfigurationCountableConnection(CountableConnection):
+    class Meta:
+        node = TaxConfiguration
