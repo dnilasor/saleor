fe0ce4b419fc37425f86bd99c3f4cc1c3ca820c2
dnilasor <rosalind26@gmail.com>
2022-05-25 17:39:23 -0700

    add executable

--- render-start.sh
+++ render-start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 set -e # exit on error
 ALLOWED_HOSTS=$RENDER_EXTERNAL_HOSTNAME
+
+gunicorn --bind :$PORT --workers 4 --worker-class uvicorn.workers.UvicornWorker saleor.asgi:application
