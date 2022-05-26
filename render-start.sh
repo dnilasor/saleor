7cbd4866ec838cf3bd5bfcd888e9ca55cbafa495
dnilasor <rosalind26@gmail.com>
2022-05-25 18:10:19 -0700

    debug allowed hosts

--- render-start.sh
+++ render-start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e # exit on error
-ALLOWED_HOSTS=$RENDER_EXTERNAL_HOSTNAME
+ALLOWED_HOSTS=".onrender.com"
 
+echo $ALLOWED_HOSTS
 gunicorn --bind :$PORT --workers 4 --worker-class uvicorn.workers.UvicornWorker saleor.asgi:application
