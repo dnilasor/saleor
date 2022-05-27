626aaddb3897fbcd346b3c6c6f3ac835b31b375e
dnilasor <rosalind26@gmail.com>
2022-05-27 16:20:52 -0700

    set env var in start script too

--- render-start.sh
+++ render-start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e # exit on error
 
+export RSA_PRIVATE_KEY=$(cat /etc/secrets/saleor-key)
+
 python -c 'import os; print(os.environ.get("ALLOWED_HOSTS"))'
 gunicorn --bind :$PORT --workers 4 --worker-class uvicorn.workers.UvicornWorker saleor.asgi:applicationg
