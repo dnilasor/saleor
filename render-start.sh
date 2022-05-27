842b6ad8217684166c8d4a1325f69f11eafa5a83
dnilasor <rosalind26@gmail.com>
2022-05-27 15:38:26 -0700

    set rsa priv key to secret file contents

--- render-start.sh
+++ render-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e # exit on error
 
-export FOO=$(cat .my-multiline-env-var)
+export RSA_PRIVATE_KEY=$(cat /etc/secrets/saleor-key)
 
 python -c 'import os; print(os.environ.get("ALLOWED_HOSTS"))'
 gunicorn --bind :$PORT --workers 4 --worker-class uvicorn.workers.UvicornWorker saleor.asgi:applicationg
