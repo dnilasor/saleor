cf9851c02de98adb0efe0d1033a8ca4bf497f69d
Chris Castle <chris@render.com>
2022-06-24 18:51:09 -0700

    Update build script

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e # exit on error
 
+export RSA_PRIVATE_KEY=$(cat /etc/secrets/saleor-key)
+
 pip3 install -r requirements.txt
 
-python manage.py migrate --no-input
+if [ "$RENDER_SERVICE_TYPE" = "web" ]; then
+  python manage.py migrate --no-input
+fi
